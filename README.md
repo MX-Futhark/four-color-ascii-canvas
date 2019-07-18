3d82e6f38249ad59f9923574784a085a8f8d52cc
Maxime Pia <maxime.pia@neuf.fr>
2019-07-18 13:28:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# four-color-ascii-canvas
+
+## Usage
+
+```js
+var FourColorASCIICanvas = require('four-color-ascii-canvas');
+var canvas = new FourColorASCIICanvas(WIDTH, HEIGHT);
+canvas.paint(ndarray); // Flat array of RGBA pixels of length WIDTH*HEIGHT*4
+```
---
+++ index.js
@@ -0,0 +1,95 @@
+const cursor = require('ansi')(process.stdout);
+
+module.exports = class FourColorASCIICanvas {
+
+    constructor (width, height) {
+        this._width = width;
+        this._height = height;
+
+        this._previousPixels = [];
+    }
+
+    static get PALETTE () {
+        // One character = two pixels aligned vertically.
+        // The characters below are chosen based on the pixel density of their high and low parts,
+        // so that they can be mapped to a range of RGB colors based on their brightness.
+        return [
+            [' ', '.', '+', 'u'],
+            ['`', ':', 'L', 'd'],
+            ['"', 'T', 'I', '6'],
+            ['*', 'P', '9', '8']
+        ];
+    }
+
+    paint (pixels) {
+
+        const getBrighnessLevel = (ndarray, index) => {
+            const r = ndarray[index + 0];
+            const g = ndarray[index + 1];
+            const b = ndarray[index + 2];
+
+            // luminance-based
+            return (0.299 * r + 0.587 * g + 0.114 * b) / 64 | 0;
+        };
+
+        const getChar = (ndarray, topPixelIndex, bottomPixelIndex) => {
+            const topBrightnessLevel = getBrighnessLevel(ndarray, topPixelIndex);
+            const bottomBrightnessLevel = getBrighnessLevel(ndarray, bottomPixelIndex);
+
+            return FourColorASCIICanvas.PALETTE[topBrightnessLevel][bottomBrightnessLevel];
+        };
+
+        const canvasLength = this._width * this._height;
+
+        if (pixels.length !== canvasLength * 4) {
+            throw new Error('Pixel array does not match the dimensions of the canvas');
+        }
+
+        let sameCharCounter = 0;
+
+        cursor
+            .buffer()
+            .goto(1, 1);
+
+        for (let j = 0; j < this._height; j += 2) {
+            for (let i = 0; i < this._width; ++i) {
+                const topPixelIndex = (j * this._width + i) * 4;
+                const bottomPixelIndex = ((j + 1) * this._width + i) * 4;
+                const char = getChar(pixels, topPixelIndex, bottomPixelIndex);
+                const previousFrameChar = getChar(this._previousPixels, topPixelIndex, bottomPixelIndex);
+
+                if (char === previousFrameChar) {
+                    ++sameCharCounter;
+                } else {
+                    const x = i + 1;
+                    const y = j / 2 + 1;
+                    const gotoXY = `\x1b[${x};${y}H`;
+
+                    // Do not send a goto instruction if is is more costly than re-writing the characters
+                    if (sameCharCounter > gotoXY.length) {
+                        cursor._buffer.splice(cursor._buffer.length - sameCharCounter);
+                        cursor.goto(x, y);
+                    }
+
+                    sameCharCounter = 0;
+                }
+
+                cursor.write(char);
+            }
+
+            ++sameCharCounter;
+            cursor.write('\n');
+        }
+
+        if (sameCharCounter) {
+            cursor._buffer.splice(cursor._buffer.length - sameCharCounter);
+        }
+
+        // Avoid leaving the cursor in the middle of the canvas
+        cursor
+            .goto(this._width + 1, this._height / 2)
+            .flush();
+
+        this._previousPixels = pixels;
+    }
+}
